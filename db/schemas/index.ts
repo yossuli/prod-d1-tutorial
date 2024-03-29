86e66f38fd43c8a51581589018ea59df3bedf465
yossuli <s1f102302348@iniad.org>
2024-03-29 22:24:46 +0900

    chore fit lint rule

--- db/schemas/index.ts
+++ db/schemas/index.ts
@@ -1,36 +1,36 @@
-import { sql } from "drizzle-orm";
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sql } from 'drizzle-orm'
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 
-export type * from "./type";
-export const todoStatusEnum: ["todo", "doing", "done"] = [
-  "todo",
-  "doing",
-  "done",
-];
+export type * from './type'
+export const todoStatusEnum: ['todo', 'doing', 'done'] = [
+  'todo',
+  'doing',
+  'done',
+]
 
-export const todos = sqliteTable("todo", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  title: text("title").notNull(),
-  status: text("status", { enum: todoStatusEnum }).default("todo").notNull(),
-  createdAt: integer("created_at", { mode: "timestamp" })
+export const todos = sqliteTable('todo', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  title: text('title').notNull(),
+  status: text('status', { enum: todoStatusEnum }).default('todo').notNull(),
+  createdAt: integer('created_at', { mode: 'timestamp' })
     .default(sql`(strftime('%s', 'now'))`)
     .notNull(),
-  updatedAt: integer("updated_at", { mode: "timestamp" })
+  updatedAt: integer('updated_at', { mode: 'timestamp' })
     .default(sql`(strftime('%s', 'now'))`)
     .notNull(),
-  createdBy: integer("createdBy"),
-});
+  createdBy: integer('createdBy'),
+})
 
-export const users = sqliteTable("user", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  accountId: text("accountId").unique().notNull(),
-  password: text("password").notNull(),
-  name: text("name").notNull(),
-  sessionId: text("sessionId").unique().notNull(),
-  lastLoggedInAt: integer("lastLoggedInAt", { mode: "timestamp" })
+export const users = sqliteTable('user', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  accountId: text('accountId').unique().notNull(),
+  password: text('password').notNull(),
+  name: text('name').notNull(),
+  sessionId: text('sessionId').unique().notNull(),
+  lastLoggedInAt: integer('lastLoggedInAt', { mode: 'timestamp' })
     .default(sql`(strftime('%s', 'now'))`)
     .notNull(),
-  isLoggedIn: integer("isLoggedIn", { mode: "boolean" })
+  isLoggedIn: integer('isLoggedIn', { mode: 'boolean' })
     .notNull()
     .default(true),
-});
+})
