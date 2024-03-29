8f9ad6bc35d741b3555ae3f51feca887855e658a
yossuli <s1f102302348@iniad.org>
2024-03-29 22:25:19 +0900

    chore fit lint rule

--- db/schemas/type.ts
+++ db/schemas/type.ts
@@ -1,8 +1,8 @@
-import type { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import type { todos, users } from ".";
+import type { todos, users } from '.'
+import type { InferInsertModel, InferSelectModel } from 'drizzle-orm'
 
-export type SelectTodo = InferSelectModel<typeof todos>;
-export type InsertTodo = InferInsertModel<typeof todos>;
+export type SelectTodo = InferSelectModel<typeof todos>
+export type InsertTodo = InferInsertModel<typeof todos>
 
-export type SelectUser = InferSelectModel<typeof users>;
-export type InsertUser = InferInsertModel<typeof users>;
+export type SelectUser = InferSelectModel<typeof users>
+export type InsertUser = InferInsertModel<typeof users>
