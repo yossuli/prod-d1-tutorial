02d824ab384ca87a4521d4f162d99a7cff6647ef
yossuli <s1f102302348@iniad.org>
2024-03-29 22:25:33 +0900

    chore fit lint rule

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,29 @@
-import pages from "@hono/vite-cloudflare-pages";
-import pagesPlugin from "@hono/vite-dev-server/cloudflare-pages";
-import honox from "honox/vite";
-import client from "honox/vite/client";
-import { defineConfig } from "vite";
+import pages from '@hono/vite-cloudflare-pages'
+import pagesPlugin from '@hono/vite-dev-server/cloudflare-pages'
+import honox from 'honox/vite'
+import client from 'honox/vite/client'
+import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => {
-  if (mode === "client") {
+  if (mode === 'client') {
     return {
       plugins: [client()],
-    };
-  } else {
-    return {
-      plugins: [
-        honox({
-          devServer: {
-            entry: "./app/server.ts",
-            plugins: [
-              pagesPlugin({
-                d1Databases: ["DB"],
-                d1Persist: true,
-              }),
-            ],
-          },
-        }),
-        pages(),
-      ],
-    };
+    }
   }
-});
+  return {
+    plugins: [
+      honox({
+        devServer: {
+          entry: './app/server.ts',
+          plugins: [
+            pagesPlugin({
+              d1Databases: ['DB'],
+              d1Persist: true,
+            }),
+          ],
+        },
+      }),
+      pages(),
+    ],
+  }
+})
