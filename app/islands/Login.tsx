de8af7e1073a5c6ef0c7bbf34182e0da0a5c0e7b
yossuli <s1f102302348@iniad.org>
2024-04-02 11:45:35 +0900

    update login form

--- app/islands/Login.tsx
+++ app/islands/Login.tsx
@@ -1,29 +1,45 @@
 import { css } from 'hono/css'
-import { useState } from 'hono/jsx'
+import { useRef, useState } from 'hono/jsx'
 
 const roleHandler = (isSignIn: boolean) => (isSignIn ? 'sign in' : 'log in ')
 export default function Login() {
-  const [isSignIn, changeRole] = useState(true)
+  const [isRoleSignIn, changeRole] = useState(true)
+  const formRef = useRef<HTMLFormElement | null>(null)
   const submit = (e: Event) => {
     const inputs = Array.from(document.getElementsByTagName('input'))
+    console.log(inputs)
     const isInBlankElm = inputs
-      .filter(e => e.type !== 'submit' && (e.name !== 'name' || !isSignIn))
-      .some(e => e.value !== '')
+      .filter(e => e.name !== 'name' || isRoleSignIn)
+      .some(e => e.value === '')
     if (isInBlankElm) {
       alert('please input all element')
       e.preventDefault()
     }
   }
+  const clickChangeRoleButton = (e: Event) => {
+    changeRole(c => !c)
+    e.preventDefault()
+  }
+  console.log(formRef.current?.clientHeight)
   return (
     <>
       <form
         class={css`
         display: grid;
-        grid-template-columns: repeat(2, auto);
+        grid-template-columns: repeat(2, "fit-content");
+        height: ${
+          formRef !== null
+            ? `${formRef.current?.clientHeight}px`
+            : 'fit-content'
+        };
+        max-width: 480px;
+        gap: 0.5rem;
+        /* background-color: red; */
       `}
         method='POST'
+        ref={formRef}
       >
-        {isSignIn && (
+        {isRoleSignIn && (
           <>
             <label for='name'>name</label>
             <input type='text' name='name' />
@@ -33,7 +49,7 @@ export default function Login() {
         <input type='text' name='accountId' />
         <label for='password'>password</label>
         <input type='password' name='password' />
-        <input
+        <button
           type='submit'
           class={css`
           grid-column: 1/3;
@@ -41,11 +57,21 @@ export default function Login() {
           margin: auto;
         `}
           onClick={submit}
-        />
+        >
+          {roleHandler(isRoleSignIn)}
+        </button>
+        <button
+          type='button'
+          onClick={clickChangeRoleButton}
+          class={css`
+          grid-column: 1/3;
+          width: fit-content;
+          margin: auto;
+        `}
+        >
+          change to {roleHandler(!isRoleSignIn)}
+        </button>
       </form>
-      <button onClick={() => changeRole(!isSignIn)}>
-        {roleHandler(!isSignIn)}
-      </button>
     </>
   )
 }
