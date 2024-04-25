d15ce876e667104a7cabda5c5ea260af4c244b05
yossuli <s1f102302348@iniad.org>
2024-04-25 11:56:37 +0900

    update: logout use useCase

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,9 +1,7 @@
-import { and, eq } from 'drizzle-orm'
-import { drizzle } from 'drizzle-orm/d1'
 import { deleteCookie, getCookie } from 'hono/cookie'
 import { createRoute } from 'honox/factory'
 
-import { users } from '../../db/schemas'
+import { userUseCase } from '../useCase/userUseCase'
 
 import type { Res } from './types'
 import type { SelectUser } from '../../db/schemas'
@@ -11,29 +9,21 @@ import type { SelectUser } from '../../db/schemas'
 export const GET = createRoute(async c => {
   const sessionId = getCookie(c, 'sessionId')
   if (sessionId !== undefined) {
-    const db = drizzle(c.env.DB)
-    const res: Res<SelectUser> = await db
-      .update(users)
-      .set({
-        isLoggedIn: false,
-        sessionId: undefined,
-      })
-      .where(and(eq(users.sessionId, sessionId)))
-      .returning()
-      .then((v): { status: 200; body: SelectUser } => ({
-        status: 200,
-        body: v[0],
-      }))
-      .catch(() => ({ status: 500 }))
+    const db = new userUseCase(c)
+    const res: Res<SelectUser, [200 | 400 | 500 | 401]> =
+      await db.logout(sessionId)
     if (res.status === 200) {
       deleteCookie(c, 'sessionId')
       return c.redirect('/')
     }
+    if (res.status === 401) {
+      deleteCookie(c, 'sessionId')
+    }
+    return c.render(
+      <dev>
+        <h1>Error Occurred!</h1>
+        <a href='/'>redirect to home</a>
+      </dev>,
+    )
   }
-  return c.render(
-    <dev>
-      <h1>Error Occurred!</h1>
-      <a href='/'>redirect to home</a>
-    </dev>,
-  )
 })
